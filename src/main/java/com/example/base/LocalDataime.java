e7ef20e08f24fb52725b500b538707bab74ea6c3
阮克锐 <rkr@strongsoft.net>
2023-09-28 10:24:16 +0800

    线程测试添加

---
+++ src/main/java/com/example/base/LocalDataime.java
@@ -0,0 +1,39 @@
+package com.example.base;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class LocalDataime {
+
+    public static void main(String[] args) {
+       test();
+    }
+
+
+
+
+    public static void test() {
+        //判断两个时间点的前后
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime end=now.withDayOfMonth(3).withHour(13).withMinute(30).withSecond(0);
+        System.out.println("end="+ end.format(DateTimeFormatter.ISO_DATE_TIME));
+
+        LocalDateTime target = LocalDateTime.of(2022, 2, 22, 22, 22, 22);
+        boolean isBefore = now.isBefore(target);
+        System.out.println("now:"+now);
+        System.out.println("target:"+target);
+        System.out.println("isBefore:"+isBefore);
+        System.out.println(LocalDate.now().isBefore(LocalDate.of(2022, 2, 22)));
+        System.out.println(LocalTime.now().isAfter(LocalTime.parse("08:15:00")));
+
+        // 时间转化为Datetime
+
+        Date date=Date.from(end.atZone(ZoneId.of("UTC-8")).toInstant());
+        System.out.println("date= "+ date);
+
+    }
+}
---
+++ src/main/java/com/example/thread/CompeletableFutureTest.java
@@ -0,0 +1,13 @@
+package com.example.thread;
+
+/**
+ * 异步线程测试
+ */
+public class CompeletableFutureTest {
+
+    public static void main(String[] args) {
+      // https://www.cnblogs.com/ludongguoa/p/15316488.html
+
+    }
+
+}
